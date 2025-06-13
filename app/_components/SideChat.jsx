be3ccb3335c682731ddb790ae6d6b1cb03b47a0e
unknown <huzefaratlam63@gmail.com>
2025-06-13 18:42:43 +0530

    y

--- app/_components/SideChat.jsx
+++ app/_components/SideChat.jsx
@@ -8,6 +8,7 @@ import axios from "axios";
 import AddFriend from "./AddFriend";
 import { Poppins } from "next/font/google";
 import { UseSocketContext } from "./SocketProvider";
+import Spinner from "./Spinner";
 const p = Poppins({
   subsets: ["latin"],
   variable: "p",
@@ -19,7 +20,7 @@ function SideChat({userTypingId}) {
   const searchParams = useSearchParams();
   const {socket} = UseSocketContext();
   const queryClient = useQueryClient();
-  const {data:chats,isPending} = useQuery({
+  const {data:chats,isFetching} = useQuery({
     queryKey:['chats'],
     queryFn:getChats,
     refetchOnWindowFocus:false
@@ -45,27 +46,37 @@ function SideChat({userTypingId}) {
     }
   }
    return (
-    <div className={`h-full ${searchParams.get('friendId') && 'hidden lg:flex'} min-w-full lg:max-w-[25%] lg:min-w-[25%] relative bg-[var(--background)] shadow-sm flex flex-col border-r-1 border-[var(--border)]`}>
-      <h1 className="text-center mt-3 text-3xl text-[var(--text)]">WHISPR</h1>
-      <AddFriend />
-      {chats?.chats && chats?.chats?.length < 1 && (
-        <div className={`${p.className} absolute left-1/2 text-center  text-[var(--text)] flex flex-col gap-3 top-1/2 -translate-1/2 w-full`}>
-          <p className={` w-full `}>you don't have any friends added</p>
-          <p>add friends to start chatting</p>
-        </div>
-      )}
-      <div className="flex flex-col gap-2 items-center border-[var(--border)] w-full flex-1 overflow-auto mt-3 py-3">
-        {chats?.chats?.map((el, i) => (
-          <SideChatProfile
-          userTypingId={userTypingId}
-            key={i}
-            chat={el}
-            currentUserId={chats?.currentUserId}
-          />
-        ))}
-      </div>
-    </div>
-  );
+     <div
+       className={`h-full ${
+         searchParams.get("friendId") && "hidden lg:flex"
+       } min-w-full lg:max-w-[25%] lg:min-w-[25%] relative bg-[var(--background)] shadow-sm flex flex-col border-r-1 border-[var(--border)]`}
+     >
+       <h1 className="text-center mt-3 text-3xl text-[var(--text)]">WHISPR</h1>
+       <AddFriend />
+       {chats?.chats && chats?.chats?.length < 1 && (
+         <div
+           className={`${p.className} absolute left-1/2 text-center  text-[var(--text)] flex flex-col gap-3 top-1/2 -translate-1/2 w-full`}
+         >
+           <p className={` w-full `}>you don't have any friends added</p>
+           <p>add friends to start chatting</p>
+         </div>
+       )}
+       {!isFetching ? (
+         <div className="flex flex-col gap-2 items-center border-[var(--border)] w-full flex-1 overflow-auto mt-3 py-3">
+           {chats?.chats?.map((el, i) => (
+             <SideChatProfile
+               userTypingId={userTypingId}
+               key={i}
+               chat={el}
+               currentUserId={chats?.currentUserId}
+             />
+           ))}
+         </div>
+       ) : (
+         <Spinner />
+       )}
+     </div>
+   );
 }
 
 export default SideChat;
