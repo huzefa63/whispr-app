96f1e88254c539438118bc67b7daeaeb84e5b438
unknown <huzefaratlam63@gmail.com>
2025-08-11 12:15:47 +0530

    added showing dates of messages

--- app/_components/ChatContainer.jsx
+++ app/_components/ChatContainer.jsx
@@ -5,16 +5,17 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import Spinner from "./Spinner";
-import { BiCheckDouble,BiCheck } from "react-icons/bi";
+import { BiCheckDouble, BiCheck } from "react-icons/bi";
 
 import AudioPlayer from "react-h5-audio-player";
 import { useGlobalState } from "./GlobalStateProvider";
-function ChatContainer({chats, containerRef,params}) {
+import { MdModeEditOutline } from "react-icons/md";
+function ChatContainer({ chats, containerRef, params }) {
   const { messages, setMessages, scroll, setScroll, friendId, setFriendId } =
     useGlobalState();
   // const [scroll, setScroll] = useState(true);
   const queryClient = useQueryClient();
-  const { data, isLoading,isFetching } = useQuery({
+  const { data, isLoading, isFetching } = useQuery({
     queryKey: ["messages", params],
     queryFn: () => getMessages(params),
     refetchOnWindowFocus: false,
@@ -40,13 +41,11 @@ function ChatContainer({chats, containerRef,params}) {
     }
   }
 
-  
-
   useEffect(() => {
     setFriendId(Number(params));
-    
+
     const jwt = localStorage.getItem("jwt");
-    if(!jwt) return;
+    if (!jwt) return;
     const userId = jwtDecode(jwt)?.id;
     async function markRead() {
       const res2 = await axios.get(
@@ -59,30 +58,47 @@ function ChatContainer({chats, containerRef,params}) {
       );
     }
     markRead();
-    queryClient.setQueryData(['chats'],(previousChats) => {   
-      if(previousChats?.chats?.length < 1 || !previousChats) return previousChats;
-      const index = previousChats?.chats?.findIndex(el => (el?.userId == userId && el?.user2Id == params) || (el?.userId == params && el?.user2Id == userId))
-    
-      if(index === -1) return previousChats
-      if(previousChats?.chats[index]?.isRecentMessageRead || previousChats?.chats[index]?.recentMessageSenderId == userId) return previousChats;
+    queryClient.setQueryData(["chats"], (previousChats) => {
+      if (previousChats?.chats?.length < 1 || !previousChats)
+        return previousChats;
+      const index = previousChats?.chats?.findIndex(
+        (el) =>
+          (el?.userId == userId && el?.user2Id == params) ||
+          (el?.userId == params && el?.user2Id == userId)
+      );
+
+      if (index === -1) return previousChats;
+      if (
+        previousChats?.chats[index]?.isRecentMessageRead ||
+        previousChats?.chats[index]?.recentMessageSenderId == userId
+      )
+        return previousChats;
       const copy = [...previousChats?.chats];
       copy[index].isRecentMessageRead = true;
-      return {...previousChats,chats:copy}
-    })
+      return { ...previousChats, chats: copy };
+    });
   }, [params]);
 
-  useEffect(()=>{
-    if(containerRef.current && !isFetching){
+  useEffect(() => {
+    if (containerRef.current && !isFetching) {
       containerRef.current.scrollTop = containerRef.current.scrollHeight;
     }
-  },[params,isFetching,scroll])
+  }, [params, isFetching, scroll]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const jwt = localStorage.getItem("jwt");
-    if(!jwt) return;
+    if (!jwt) return;
     const userId = jwtDecode(jwt);
-    if(messages[messages.length - 1]?.senderId == userId || friendId != (messages[messages.length - 1]?.senderId)) return;
-    if(messages[messages.length - 1]?.senderId == friendId && messages[messages.length - 1]?.isRead === true) return;
+    if (
+      messages[messages.length - 1]?.senderId == userId ||
+      friendId != messages[messages.length - 1]?.senderId
+    )
+      return;
+    if (
+      messages[messages.length - 1]?.senderId == friendId &&
+      messages[messages.length - 1]?.isRead === true
+    )
+      return;
     async function markRead() {
       const res2 = await axios.get(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/message/readMessages/${params}`,
@@ -94,24 +110,25 @@ function ChatContainer({chats, containerRef,params}) {
       );
     }
     markRead();
-  },[messages?.length,params])
+  }, [messages?.length, params]);
   useEffect(() => {
-    const jwt = localStorage.getItem('jwt');
-    if(!jwt) return;
+    const jwt = localStorage.getItem("jwt");
+    if (!jwt) return;
     const currentUserId = jwtDecode(jwt)?.id;
-      if (
-        ((containerRef.current &&
-          scroll &&
-          messages[messages.length - 1]?.Type === "image" || messages[messages.length -1]?.Type === 'audio') )||
-        messages[messages.length - 1]?.placeholder || 
-        messages[messages.length - 1]?.senderId !== currentUserId
-      ) {
-        containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    if (
+      (containerRef.current &&
+        scroll &&
+        messages[messages.length - 1]?.Type === "image") ||
+      messages[messages.length - 1]?.Type === "audio" ||
+      messages[messages.length - 1]?.placeholder ||
+      messages[messages.length - 1]?.senderId !== currentUserId
+    ) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
       // setScroll(false);
-
-      }
-  }, [scroll,messages?.length]);
+    }
+  }, [scroll, messages?.length]);
   // pb-[calc(130px+env(safe-area-inset-bottom))]
+
   return (
     <div
       ref={containerRef}
@@ -119,25 +136,67 @@ function ChatContainer({chats, containerRef,params}) {
       // className="bg-[var(--surface)] pt-[20%] pb-[calc(130px+env(safe-area-inset-bottom))]  lg:pb-7 lg:pt-5 relative h-full overflow-auto text-[var(--text)] px-5 flex flex-col gap-3"
     >
       {isFetching && <Spinner />}
-      {!isFetching && chats?.length > 0 &&
-        messages?.map((el, i) => (
-          <Message key={i} message={el} setScroll={setScroll} />
+      {!isFetching &&
+        chats?.length > 0 &&
+        messages?.map((el, i, arr) => (
+          <div key={i}>
+            {i === 0 && <ShowDate dateString={el?.time} />}
+            {i !== 0 &&
+              (new Date(el?.time).getDate() !==
+                new Date(arr[i - 1]?.time).getDate() ||
+                new Date(el?.time).getMonth() !==
+                  new Date(arr[i - 1]?.time).getMonth() ||
+                new Date(el?.time).getFullYear() !==
+                  new Date(arr[i - 1]?.time).getFullYear()) && (
+                <ShowDate dateString={el?.time} />
+              )}
+            <Message key={i} message={el} setScroll={setScroll} />
+          </div>
         ))}
       {!isFetching && messages?.length < 1 && (
-        
-          <div className="text-center flex items-center text-lg lg:text-xl flex-col justify-center text-gray-400 mt-5  h-full">
-            <p className="">
-              This chat is{" "}
-              <span className="font-semibold text-gray-300">
-                end-to-end encrypted
-              </span>
-              .
-            </p>
-            <p>Only you and your contact can read the messages.</p>
-            <p className="mt-2 italic">Start the conversation securely.</p>
-          </div>
-        
+        <div className="text-center flex items-center text-lg lg:text-xl flex-col justify-center text-gray-400 mt-5  h-full">
+          <p className="">
+            This chat is{" "}
+            <span className="font-semibold text-gray-300">
+              end-to-end encrypted
+            </span>
+            .
+          </p>
+          <p>Only you and your contact can read the messages.</p>
+          <p className="mt-2 italic">Start the conversation securely.</p>
+        </div>
       )}
+    </div>
+  );
+}
+
+function ShowDate({ dateString }) {
+  // console.log(dateString);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const date = ` ${new Date(dateString).getDate()} ${
+    months[new Date(dateString).getMonth()]
+  }, ${new Date(dateString).getFullYear()} `;
+  const day = new Date(dateString).getDate();
+  const month = new Date(dateString).getMonth();
+  const year = new Date(dateString).getFullYear();
+  const toadyDate = new Date();
+  const isToday = (toadyDate.getDate() === day) && (toadyDate.getMonth() === month) && (toadyDate.getFullYear() === year); 
+  return (
+    <div className="w-full text-sm lg:text-md flex justify-center items-center">
+      <p className="bg-[var(--muted)] py-2 px-4 rounded-md shadow-sm">{isToday ? 'Today' : date}</p>
     </div>
   );
 }
@@ -147,13 +206,14 @@ export default ChatContainer;
 function Message({ message, setScroll }) {
   if (typeof window === "undefined") return;
   const token = localStorage.getItem("jwt");
-  if(!token) return;
+  if (!token) return;
   const currentUserId = jwtDecode(token)?.id;
 
   const time = format(new Date(message?.time), "HH:mm");
   if (message.Type === "text")
     return (
       <div className="w-full">
+        {/* <MdModeEditOutline /> */}
         <p
           className={`relative rounded-sm pl-3 pr-20 p-2 w-fit max-w-3/4 lg:max-w-[45%]  break-words ${
             currentUserId === Number(message?.senderId)
@@ -187,7 +247,6 @@ function Message({ message, setScroll }) {
       </div>
     );
   if (message.Type === "image")
-    
     return (
       <div className="w-full gap-2 ">
         <div
@@ -207,7 +266,7 @@ function Message({ message, setScroll }) {
             }  absolute flex gap-1 items-center`}
           >
             {time}
-            {message?.senderId === currentUserId &&  (
+            {message?.senderId === currentUserId && (
               <BiCheckDouble
                 className={`text-lg ${message?.isRead && "text-blue-400"}`}
               />
@@ -217,7 +276,6 @@ function Message({ message, setScroll }) {
       </div>
     );
   if (message.Type === "audio")
-    
     return (
       <div className="w-full">
         <div
@@ -227,7 +285,11 @@ function Message({ message, setScroll }) {
         >
           <AudioPlayer
             src={message?.mediaUrl}
-            className={message?.senderId === currentUserId ? 'audio-player audio-sended':'audio-player audio-recieved'}
+            className={
+              message?.senderId === currentUserId
+                ? "audio-player audio-sended"
+                : "audio-player audio-recieved"
+            }
             layout="horizontal-reverse"
             defaultDuration={["00:00"]}
             defaultCurrentTime={["00:00"]}
